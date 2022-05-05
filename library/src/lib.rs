07d7ba47a1a72dc47d44f0c6a544cd2ce1486c1d
Rico-bilb <mburueric89@gmail.com>
2022-05-05 15:54:15 +0300

    third

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -8,7 +8,6 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        // --snip--
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -21,7 +20,6 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // --snip--
     let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
