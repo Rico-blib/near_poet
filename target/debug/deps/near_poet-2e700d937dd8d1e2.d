c49757f7e9f3f6bbf549fb70d5601c0560ebd79d
Rico-bilb <mburueric89@gmail.com>
2022-05-05 15:50:14 +0300

    second

---
+++ target/debug/deps/near_poet-2e700d937dd8d1e2.d
@@ -0,0 +1,5 @@
+/home/ericka/MyWork/near_poet/target/debug/deps/near_poet-2e700d937dd8d1e2: src/main.rs
+
+/home/ericka/MyWork/near_poet/target/debug/deps/near_poet-2e700d937dd8d1e2.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/near_poet.d
@@ -0,0 +1,1 @@
+/home/ericka/MyWork/near_poet/target/debug/near_poet: /home/ericka/MyWork/near_poet/src/main.rs
